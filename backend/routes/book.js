0b3fa62fd8eaa2d4f4814204cf4b9eb58a14d4fc
Tabrezhira <Tabrez.hira@gmail.com>
2025-08-11 16:54:25 +0530

    a

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -79,8 +79,8 @@ router.get('/get-recent-books', async(req, res) => {
         return res.json({status: 'Sucess', data:books})
         
     } catch (error) {
-        console.log(error)
-        res.status(500).json({message:'Internal server error get-all-books'}) 
+        
+        res.status(500).json({message:error}) 
     }
 })
 
